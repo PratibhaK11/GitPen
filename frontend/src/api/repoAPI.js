f0108db2d5b318e6660e7bc32171533c526c5707
Pratibha Kumari <kpratibha1620@gmail.com>
2024-09-16 03:56:57 +0530

    Minor Issue fixed

--- frontend/src/api/repoAPI.js
+++ frontend/src/api/repoAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3002';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Fetch repository by ID
 export const fetchRepositoryById = async (repoId) => {
