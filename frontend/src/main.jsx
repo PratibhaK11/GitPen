cdd167611eee6c3bc56a35772d55e6c2b592b2bb
Pratibha Kumari <kpratibha1620@gmail.com>
2024-09-18 07:43:38 +0530

    UI improvements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,6 @@ import { AuthProvider } from './authContext.jsx'
 import ProjectRoutes from './Routes.jsx';
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
